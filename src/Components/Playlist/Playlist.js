e7b7f886d438143f17f78b92a2ce588d4f5a064e
ma-mrl <marion.mrl@live.fr>
2024-04-16 18:56:07 +0200

    created Playlist component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,12 +1,22 @@
 import React from "react";
 import styles from "./Playlist.module.css";
+import Tracklist from "../TrackList/TrackList";
 
-function Playlist() {
+function Playlist(props) {
+    function handleNameChange ({target}) {
+      props.onNameChange(target.value)
+    }
+
   return (
-    <div className="Playlist">
-      <input defaultValue={"New Playlist"} />
+    <div className={styles.Playlist}>
+      <input defaultValue={"New Playlist"} onChange={handleNameChange} />
       {/* <!-- Add a TrackList component --> */}
-      <button className="Playlist-save">
+      <Tracklist
+        userSearchResults={props.playlistTracks}
+        onRemove={props.onRemove}
+        isRemoval={true}
+      />
+      <button className={styles["Playlist-save"]} onClick={props.onSave}>
         SAVE TO SPOTIFY
       </button>
     </div>
