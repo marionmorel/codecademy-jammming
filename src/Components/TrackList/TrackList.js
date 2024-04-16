8b5b6bb5638162942d8c286a7541f02fbf8aabdf
ma-mrl <marion.mrl@live.fr>
2024-04-16 18:50:39 +0200

    created TrackList component

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,10 +1,20 @@
 import React from "react";
 import styles from "./TrackList.module.css";
+import Track from "../Track/Track";
 
-function Tracklist () {
+function Tracklist(props) {
     return (
-        <div className="TrackList">
+        <div className={styles.Tracklist}>
         {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        {props.userSearchResults.map((track) => (
+            <Track
+                track={Track}
+                key={Track.id}
+                isRemoval={props.isRemoval}
+                onAdd={props.onAdd}
+                onRemoval={props.onRemoval}
+            />
+        ))}
       </div>
     );
 }
